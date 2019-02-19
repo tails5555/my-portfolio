4a3313634fb43f25d2f240263d0089eb25599ffb
tails5555 <tails5555@naver.com>
2019-02-19 20:04:12 +0900

    Updates

---
+++ precache-manifest.8049057d0dee6767d95d00130da86648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db7cfbadc0453775f283",
+    "url": "/prography-front/static/js/runtime~main.db7cfbad.js"
+  },
+  {
+    "revision": "babfa3d709f506d3a2c1",
+    "url": "/prography-front/static/js/main.babfa3d7.chunk.js"
+  },
+  {
+    "revision": "32e59f35a6e83eae643b",
+    "url": "/prography-front/static/js/2.32e59f35.chunk.js"
+  },
+  {
+    "revision": "babfa3d709f506d3a2c1",
+    "url": "/prography-front/static/css/main.cc08d09b.chunk.css"
+  },
+  {
+    "revision": "7b8698882ee7cc3fd5e5b90012c9b41b",
+    "url": "/prography-front/index.html"
+  }
+];
